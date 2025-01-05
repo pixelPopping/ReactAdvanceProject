4b2da2c547c15b8fe760e6b80c763f796c651b55
Yorian Fransz <syndey-cook@outlook.com>
2025-01-05 18:05:46 +0100

    add

--- starter/src/components/NavBar/NavBar.jsx
+++ starter/src/components/NavBar/NavBar.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./NavBar.module.css";
 
 export const Navigation = () => {
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles.menuItems}>
-        <li className={styles.menuItem}>
-          <Link className={styles.menuLink} to="/">
+    <nav className="navbar">
+      <ul className="menuItems">
+        <li className="menuItem">
+          <Link className="menuLink" to="/">
             Events
           </Link>
         </li>
-        <li className={styles.menuItem}>
-          <Link className={styles.menuLink} to="/event/1">
+        <li className="menuItem">
+          <Link className="menuLink" to="/event/1">
             Event
           </Link>
         </li>
